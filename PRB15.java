2bdab8f8083f0cc7aec420988ea24f20bbff3b92
Suyash Rawool <59467245+SuyashRawool@users.noreply.github.com>
2022-05-28 21:24:12 +0530

    Create PRB15.java

---
+++ PRB15.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class PRB15 {
+    
+    public static int[] customSort(int arr[]){
+        int n = arr.length;
+        int mid = n/2;
+        for(int i=0; i<mid-1; i++){
+            for(int j=0; j<mid-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }           
+        }
+        
+        for(int i=0; i<mid-1; i++){            
+            for(int j=mid; j<n-i-1; j++){                
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                   
+                }
+            }
+        }
+        return arr;
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {2,5,77,3,5,22,66,8,6,45,13};
+        System.out.println(Arrays.toString(arr));
+        arr  = customSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
